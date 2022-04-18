9d5ae40757bfa27938f74450b823ec82f8c2ac47
Ha3ik15 <77098471+Ha3ik15@users.noreply.github.com>
2022-04-18 22:13:15 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+
+/*VANTA.WAVES({
+  el: "#header",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x20202
+})
+*/
+VANTA.FOG({
+  el: "#body",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  highlightColor: 0x0,
+  midtoneColor: 0x0,
+  lowlightColor: 0xffffff,
+  baseColor: 0xeddede
+})
